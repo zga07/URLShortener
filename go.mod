85d2e2b4a84b498c9b23fe634211b1630655a1b5
Gregory Zolotarev <trashcllctr@gmail.com>
2026-03-12 22:14:43 +0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zga/URLShortener
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func main() {
+
+}
+
+func Encode(id uint64) string {
+	length := uint(len(alphabet))
+
+	for id > 0 {
+
+	}
+}
