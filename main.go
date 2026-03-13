a72c9727a8b7260f4c352e96a4b933acdedc8dbd
Gregory Zolotarev <trashcllctr@gmail.com>
2026-03-13 22:24:23 +0500

    encode func

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func main() {
-
+	fmt.Println(Encode(62))
 }
 
 func Encode(id uint64) string {
-	length := uint(len(alphabet))
-
+	length := uint64(len(alphabet))
+	var sbuilder strings.Builder
 	for id > 0 {
-
+		remainder := id % length
+		sbuilder.WriteByte(alphabet[remainder])
+		id /= length
 	}
+	res := sbuilder.String()
+	runeSlice := []rune(res)
+	for i, j := 0, len(runeSlice)-1; i < j; i, j = i+1, j-1 {
+		runeSlice[i], runeSlice[j] = runeSlice[j], runeSlice[i]
+	}
+	return string(runeSlice)
 }
